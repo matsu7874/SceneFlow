3c9510a08d94a639fd2d6205561269347cc92c0f
matsu7874 <mtsmtkmt@gmail.com>
2026-05-29 22:58:25 +0900

    feat(consistency): エンジンのindexを追加

---
+++ src/modules/consistency/index.ts
@@ -0,0 +1,12 @@
+export { analyzeStory } from './checker'
+export { ACT_KINDS, getActKind } from './actKinds'
+export type { ActKindValue, ActKindDef } from './actKinds'
+export type {
+  Breakage,
+  ConsistencyReport,
+  DependencyEdge,
+  DiagnosticCategory,
+  FactRef,
+  GraphNode,
+  NodeId,
+} from './types'
